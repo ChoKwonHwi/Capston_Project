615e1a3e512f4f2050dc8a4b16a35b7f40eeee5f
yseojin1@gmail.com <yseojin1@gmail.com>
2023-05-15 21:17:22 +0900

    add Lab.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import {Route, Routes} from 'react-router-dom';
 import Home from './pages/Home.js';
 import Predict from './pages/Predict.js';
 import Check from './pages/Check.js';
+import Labs from './pages/Labs.js';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/predict" element={<Predict />} />
           <Route path="/check" element={<Check />} />
-        
+          <Route path="/labs" element={<Labs />} />
       </Routes>
     </div>
   );
