c03fd9595e8bddfc92f0b327b00de81f5637c337
yseojin1@gmail.com <yseojin1@gmail.com>
2023-05-20 05:37:06 +0900

    update Home.js

--- frontend/src/pages/Check.js
+++ frontend/src/pages/Check.js
@@ -1,12 +1,28 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+
 import Header from "./Header";
 
 const Check = () => {
-    return(
-      <div>
-        <Header></Header>
-        
-      </div>
-    );
-  };
-  
-  export default Check;
+  const [searchParams] = useSearchParams();
+
+  const days = searchParams.get('days');
+  const gender = searchParams.get('gender');
+  const height = searchParams.get('height');
+  const weight = searchParams.get('weight');
+
+  // Rest of the component code
+
+  return (
+    <div>
+      <Header></Header>
+      <h1>Check</h1>
+      <p>Days: {days}</p>
+      <p>Gender: {gender}</p>
+      <p>Height: {height}</p>
+      <p>Weight: {weight}</p>
+    </div>
+  );
+};
+
+export default Check;
