0bcc6e8168fd16139c37fa32a74eb91039728dd8
안시은 <tldms5505@gmail.com>
2023-05-20 23:44:10 +0900

    sndpage update

---
+++ Backend/sndpage/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+class ResultSerializer(serializers.Serializer):
+    height_percent = serializers.FloatField()
+    weight_percent = serializers.FloatField()
+    plot_std_height = serializers.FloatField()
+    plot_std_weight = serializers.FloatField()
+    plot_data_mean_height = serializers.FloatField()
+    plot_data_mean_weight = serializers.FloatField()
+    height = serializers.FloatField()
+    weight = serializers.FloatField()
