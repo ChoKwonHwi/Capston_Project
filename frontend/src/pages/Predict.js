3e6d1c427a8f3b5aa455b0b7f625c628c2ecb752
yseojin1@gmail.com <yseojin1@gmail.com>
2023-05-30 22:04:04 +0900

    Fix Predict page

--- frontend/src/pages/Predict.js
+++ frontend/src/pages/Predict.js
@@ -5,25 +5,35 @@ import { Line } from 'react-chartjs-2';
 import Header from './Header';
 
 const Predict = () => {
-  const [heightData, setHeightData] = useState({ datasets: [] });
-  const [weightData, setWeightData] = useState({ datasets: [] });
-  const [predictionData, setPredictionData] = useState({});
-
+  const [result, setResult] = useState(null);
+  const { days, gender, height, weight } = useParams(); // useParams를 사용해서 값을 가져옴
   useEffect(() => {
-    fetchData();
-  }, []);
+    
+    axios
+      .post('http://127.0.0.1:8000/fstpage/api/predict/', {
+        days,
+        gender,
+        height,
+        weight
+      })
+      .then((response) => {
+        
+        setResult(response.data);
+      })
+      .catch((error) => {
+        
+        console.error('Error:', error);
+      });
+  }, [days, gender, height, weight]);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.post('http://127.0.0.1:8000/fstpage/api/predict/'); 
-      const data = response.data;
 
-      const heightChartData = {
-        labels: Array.from({ length: data.graph_height.length }, (_, i) => i + 1),
+  
+      const heightData = {
+        labels: Array.from({ length: result?.graph_height.length }, (_, i) => i + 1),
         datasets: [
           {
             label: '키(cm)',
-            data: data.graph_height,
+            data: result?.graph_height,
             fill: false,
             borderColor: 'blue',
             tension: 0.3,
@@ -33,12 +43,12 @@ const Predict = () => {
         ],
       };
 
-      const weightChartData = {
-        labels: Array.from({ length: data.graph_weight.length }, (_, i) => i + 1),
+      const weightData = {
+        labels: Array.from({ length: result?.graph_height.length }, (_, i) => i + 1),
         datasets: [
           {
             label: '몸무게(kg)',
-            data: data.graph_weight,
+            data: result?.graph_weight,
             fill: false,
             borderColor: 'red',
             tension: 0.3,
@@ -48,13 +58,9 @@ const Predict = () => {
         ],
       };
 
-      setHeightData(heightChartData);
-      setWeightData(weightChartData);
-      setPredictionData(data);
-    } catch (error) {
-      console.error('데이터를 불러오는 중 오류가 발생했습니다:', error);
-    }
-  };
+      
+    
+  
 
   const options = {
     scales: {
@@ -96,26 +102,34 @@ const Predict = () => {
       <div style={{ display: 'flex', justifyContent: 'space-between', margin: '60px' }}>
         <div style={{ width: '60%', marginRight: '30px' }}>
           <h3 style={{ fontFamily: 'Jua, sans-serif', textAlign: 'center' }}>&lt;예상 키 그래프&gt;</h3>
-          <Line data={heightData} options={options} />
+          {result && <Line data={heightData} options={options} />}
         </div>
         <div style={{ width: '35%' }}>
           <br></br><br></br><br></br><br></br>
-          <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 1개월 뒤 예상 키는 {predictionData.one_month_pred_height}cm 입니다.</h4>
-          <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 3개월 뒤 예상 키는 {predictionData.three_month_pred_height}cm 입니다.</h4>
-          <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 6개월 뒤 예상 키는 {predictionData.six_month_pred_height}cm 입니다.</h4>
+          {result && (
+          <>
+            <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 1개월 뒤 예상 키는 {result.one_month_pred_height}cm 입니다.</h4>
+            <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 3개월 뒤 예상 키는 {result.three_month_pred_height}cm 입니다.</h4>
+            <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 6개월 뒤 예상 키는 {result.six_month_pred_height}cm 입니다.</h4>
+          </>
+          )}
         </div>
       </div>
 
       <div style={{ display: 'flex', justifyContent: 'space-between', margin: '60px' }}>
         <div style={{ width: '60%', marginRight: '30px' }}>
           <h3 style={{ fontFamily: 'Jua, sans-serif', textAlign: 'center' }}>&lt;예상 몸무게 그래프&gt;</h3>
-          <Line data={weightData} options={weightOptions} />
+          {result && <Line data={weightData} options={weightOptions} />}
         </div>
         <div style={{ width: '35%' }}>
           <br></br><br></br><br></br><br></br>
-          <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 1개월 뒤 예상 몸무게는 {predictionData.one_month_pred_weight}kg 입니다.</h4>
-          <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 3개월 뒤 예상 몸무게는 {predictionData.three_month_pred_weight}kg 입니다.</h4>
-          <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 6개월 뒤 예상 몸무게는 {predictionData.six_month_pred_weight}kg 입니다.</h4>
+          {result && (
+          <>
+            <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 1개월 뒤 예상 몸무게는 {result.one_month_pred_weight}kg 입니다.</h4>
+            <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 3개월 뒤 예상 몸무게는 {result.three_month_pred_weight}kg 입니다.</h4>
+            <h4 style={{ fontFamily: 'Jua, sans-serif' }}>아이의 6개월 뒤 예상 몸무게는 {result.six_month_pred_weight}kg 입니다.</h4>
+          </>
+          )}
         </div>
       </div>
 
